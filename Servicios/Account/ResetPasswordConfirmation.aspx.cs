e275706bd79bedd8e821376c698f22e34d0f4c95
carlosjuliocadena <cjcs.cadenasarasty8@gmail.com>
2018-08-02 17:22:55 -0500

    Inicio Desarrollo

---
+++ Servicios/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Servicios.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Servicios/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Servicios.Startup))]
+namespace Servicios
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
